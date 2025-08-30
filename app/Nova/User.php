180c8dcc62c53dec966b8668adc2a9f41124995e
Adan Gatica <adanrobertogatica@gmail.com>
2025-08-30 20:23:17 -0300

    UserGroupTicketOnsite 4

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -131,7 +131,7 @@ class User extends Resource
             HasMany::make('Perfil Usuario', 'perfiles_usuarios'),
             BelongsToMany::make('Empresa Onsite', 'empresas_onsite'),
             BelongsToMany::make('Sucursal Onsite', 'sucursales_onsite'),
-            BelongsTo::make('Group Ticket', 'group_ticket', GroupTicketOnsite::class)->sortable(),
+            BelongsTo::make('Group Ticket', 'group_tickets', GroupTicketOnsite::class)->sortable(),
             //BelongsToMany::make('Historiales Estados Onsite', 'historiales_estados_onsite'),
             HasMany::make('HistorialEstadoOnsite', 'historial_estado_onsite'),
             HasMany::make('LocalidadOnsite', 'localidad_onsite'),
