4a97ae4740511191c5b67d794536f37b2891774b
Adan Gatica <adanrobertogatica@gmail.com>
2025-08-31 21:02:22 -0300

    UserGroupTicketOnsite 14

--- app/Nova/GroupTicketOnsite.php
+++ app/Nova/GroupTicketOnsite.php
@@ -54,10 +54,9 @@ class GroupTicketOnsite extends Resource
             BelongsTo::make('Company', 'company')->sortable(),
             Text::make('Name', 'name')->sortable()->required(),
             BelongsToMany::make('Users')
-            ->using(UserGroupTicketOnsite::class)
-            ->fields(function () {
-                return []; // no hace falta mostrar company_id
-            }),
+    ->fields(function () {
+        return []; 
+    }),
         ];
     }
 
